55bc6e22e5da62794cdbc464916cfa24ca23482c
Joshua Taylor <theofficialjoshuataylor@gmail.com>
2024-01-27 14:02:04 -0800

    Basic test for tank drive

---
+++ src/arduino/tests/test-tank-drive/test-tank-drive.ino
@@ -0,0 +1,206 @@
+// X1 WALL-E 2024
+// by Joshua Taylor
+// 2024-01-27
+
+// Designed for Teensy 4.1
+
+// Pin definitions
+#define DIR1 22
+#define PWM1 23
+#define DIR2 14
+#define PWM2 15
+
+// All variables that we're going to receive from the RPi
+short leftSpeed = 0;
+short rightSpeed = 0;
+
+short leftArmLength = 0;
+short rightArmLength = 0;
+
+short leftArmAngle = 0;
+short rightArmAngle = 0;
+
+short leftWingAngle = 0;
+short rightWingAngle = 0;
+
+short leftHandAngle = 0;
+short rightHandAngle = 0;
+
+bool bigFaceLatch = true;
+
+// Introductory message
+char intro[] = {0x55, 0x4f, 0x02, 0x39, 0x81, 0xc4};
+char terminator[] = {0x0c, 0xf7, 0x13, 0x85, 0x3f, 0x5a};
+
+// Message read status
+char status = 0;
+
+// Message type
+char type = 0;
+
+// Message data
+char data[32];
+
+// Checksum
+char checksum = 0;
+
+// Last message time
+unsigned long lastMessage = 0;
+
+void setup() {
+  
+  Serial.begin(115200);
+  Serial.println("Start");
+
+  Serial1.begin(115200);
+
+  pinMode(DIR1, OUTPUT);
+  pinMode(PWM1, OUTPUT);
+  pinMode(DIR2, OUTPUT);
+  pinMode(PWM2, OUTPUT);
+
+  digitalWrite(DIR1, LOW);
+  digitalWrite(DIR2, LOW);
+}
+
+void loop() // run over and over
+{
+  // Get message from rpi
+  if (Serial1.available())
+  {
+    lastMessage = millis();
+    // Determine what part of the message we're supposed to read
+    // 0-5: Intro
+    if (status < 6)
+    {
+      char in;
+      // Wait for the first byte of the message
+      if ((in = Serial1.read()) == intro[status])
+        status++;
+      else
+      {
+        Serial.print("BAD INTRO, ");
+        Serial.print(in);
+        Serial.print(" != ");
+        Serial.println(intro[status]);
+        status = 255;
+      }
+    }
+    // 6: Type
+    else if (status == 6)
+    {
+      type = Serial1.read();
+      status++;
+    }
+    // 7-38: Data
+    else if (status >= 7 && status < 39)
+    {
+      data[status - 7] = Serial1.read();
+      status++;
+    }
+    // 39: Checksum
+    else if (status == 39)
+    {
+      checksum = Serial1.read();
+
+      // Checksum is correct
+      // Calculate sum of all bytes in message
+      char sum = 0;
+      for (int i = 0; i < 32; i++)
+        sum += data[i];
+      
+      // Checksum is incorrect
+      if (sum != checksum)
+      {
+        Serial.println("BAD CHECKSUM");
+        status = 255;
+      }
+      else
+        status++;
+    }
+    // 40-45: Terminator
+    else if (status >= 40 && status < 46)
+    {
+      // Wait for the first byte of the message
+      if (Serial1.read() == terminator[status - 40])
+        status++;
+      else
+      {
+        Serial.println("BAD TERMINATOR");
+        status = 255;
+      }
+    }
+
+    // Complete message indicator
+    if (status == 46)
+    {
+      // Handle the message
+      if (type == 1)
+      {
+        Serial.print("GOT MESSAGE\t");
+        // Normal operation
+        leftSpeed = (data[0] << 8) | data[1];
+        Serial.print(leftSpeed);
+        Serial.print("\t");
+        rightSpeed = (data[2] << 8) | data[3];
+        Serial.println(rightSpeed);
+
+        leftArmLength = (data[4] << 8) | data[5];
+        rightArmLength = (data[6] << 8) | data[7];
+
+        leftArmAngle = (data[8] << 8) | data[9];
+        rightArmAngle = (data[10] << 8) | data[11];
+
+        leftWingAngle = (data[12] << 8) | data[13];
+        rightWingAngle = (data[14] << 8) | data[15];
+
+        leftHandAngle = (data[16] << 8) | data[17];
+        rightHandAngle = (data[18] << 8) | data[19];
+
+        bigFaceLatch = data[20] == 1;
+      }
+
+      // Reset status
+      status = 0;
+    }
+  }
+
+  // Check for timeout
+  if (millis() - lastMessage > 1000 && status != 0)
+  {
+    Serial.println("TIMEOUT");
+    status = 255;
+  }
+
+  // Bad message indicator
+  if (status == 255)
+  {
+    // Set all values to zero
+    leftSpeed = 0;
+    rightSpeed = 0;
+
+    leftArmLength = 0;
+    rightArmLength = 0;
+
+    leftArmAngle = 0;
+    rightArmAngle = 0;
+
+    leftWingAngle = 0;
+    rightWingAngle = 0;
+
+    leftHandAngle = 0;
+    rightHandAngle = 0;
+
+    bigFaceLatch = true;
+
+    // Reset status
+    Serial.println("BAD MESSAGE");
+    status = 0;
+  }
+
+  digitalWrite(DIR1, leftSpeed >= 0);
+  digitalWrite(DIR2, rightSpeed >= 0);
+
+  analogWrite(PWM1, map(abs(leftSpeed), 0, 32678, 0, 255));
+  analogWrite(PWM2, map(abs(rightSpeed), 0, 32678, 0, 255));
+}
---
+++ src/rpi/tests/test-tank-drive.py
@@ -0,0 +1,178 @@
+from pyPS4Controller.controller import Controller
+import serial
+from time import sleep
+import struct
+
+ser = serial.Serial("/dev/ttyS0", 115200)
+
+
+
+# Message variables
+intro = [0x55, 0x4f, 0x02, 0x39, 0x81, 0xc4]
+terminator = [0x0c, 0xf7, 0x13, 0x85, 0x3f, 0x5a]
+
+# Default message parameters
+leftSpeed = 0
+rightSpeed = 0
+
+leftArmLength = 0
+rightArmLength = 0
+
+leftArmAngle = 0
+rightArmAngle = 0
+
+leftWingAngle = 0
+rightWingAngle = 0
+
+leftHandAngle = 0
+rightHandAngle = 0
+
+bigFaceLatch = True
+
+def send_default_packet():
+
+	# Intro
+	for i in intro:
+		print(struct.pack('>B', i))
+		ser.write(struct.pack('>B', i))
+
+	# Type
+	print(b'\x01')
+	ser.write(b'\x01')
+	
+	# Message
+	data = b''
+	# >h = big endian short
+	data += struct.pack('>h', leftSpeed)
+	data += struct.pack('>h', rightSpeed)
+
+	data += struct.pack('>h', leftArmLength)
+	data += struct.pack('>h', rightArmLength)
+
+	data += struct.pack('>h', leftArmAngle)
+	data += struct.pack('>h', rightArmAngle)
+
+	data += struct.pack('>h', leftWingAngle)
+	data += struct.pack('>h', rightWingAngle)
+
+	data += struct.pack('>h', leftHandAngle)
+	data += struct.pack('>h', rightHandAngle)
+
+	data += struct.pack('>B', bigFaceLatch)
+
+	# fill in the rest of the 32 bytes
+	for i in range(0, 32 - len(data)):
+		data += b'\x00'
+	
+	print(data)
+	ser.write(data)
+
+	# Checksum
+	checksum = 0
+	for i in data:
+		checksum += i
+
+	checksum = checksum % 256
+	print(struct.pack('>B', checksum))
+	ser.write(struct.pack('>B', checksum))
+
+	# Terminator
+	for i in terminator:
+		print(struct.pack('>B', i))
+		ser.write(struct.pack('>B', i))
+
+# while True:
+# 	send_default_packet()
+# 	sleep(0.001)
+# 	leftSpeed += 1
+# 	rightSpeed -= 1
+
+drive_angle = 0
+drive_speed = 0
+
+last_drive_x = 0
+last_drive_y = 0
+
+def update_drive(x=None, y=None):
+	global drive_angle
+	global drive_speed
+
+	global last_drive_x
+	global last_drive_y
+
+	global leftSpeed
+	global rightSpeed
+
+	if x is not None:
+		last_drive_x = x
+	if y is not None:
+		last_drive_y = y
+
+	drive_angle = atan(last_drive_y / last_drive_x)
+	drive_speed = sqrt(last_drive_x * last_drive_x + last_drive_y * last_drive_y)
+
+	# Set left speed
+	if drive_angle >= 0 and drive_angle <= pi / 2:
+		leftSpeed = drive_speed
+	elif drive_angle >= pi and drive_angle <= 3 * pi / 2:
+		leftSpeed = -drive_speed
+	elif drive_angle > pi / 2 and drive_angle < pi:
+		# Put scale between -1 and 1
+		scale = drive_angle - (3 * pi / 4)
+		scale = scale / (pi / 4)
+		# Linearly scale speed
+		leftSpeed = scale * drive_speed
+	elif drive_angle > 3 * pi / 2 and drive_angle < 2 * pi:
+		# Put scale between -1 and 1
+		scale = drive_angle - (7 * pi / 4)
+		scale = scale / (pi / 4)
+		# Linearly scale speed
+		leftSpeed = scale * drive_speed
+
+	# Set right speed
+	if drive_angle >= pi / 2 and drive_angle <= pi:
+		rightSpeed = drive_speed
+	elif drive_angle == 0 or drive_angle >= 3 * pi / 2 and drive_angle <= 2 * pi:
+		rightSpeed = -drive_speed
+	elif drive_angle > 0 and drive_angle < pi / 2:
+		# Put scale between -1 and 1
+		scale = drive_angle - (pi / 4)
+		scale = scale / (pi / 4)
+		# Linearly scale speed
+		rightSpeed = scale * drive_speed
+	elif drive_angle > pi and drive_angle < 3 * pi / 2:
+		# Put scale between -1 and 1
+		scale = drive_angle - (5 * pi / 4)
+		scale = scale / (pi / 4)
+		# Linearly scale speed
+		rightSpeed = scale * drive_speed
+
+	send_default_packet()
+
+class MyController(Controller):
+	def __init__(self, **kwargs):
+		Controller.__init__(self, **kwargs)
+
+	# Left stick (tank drive)
+	def on_L3_up(self, value):
+		update_drive(y=value)
+	
+	def on_L3_down(self, value):
+		update_drive(y=value)
+	
+	def on_L3_left(self, value):
+		update_drive(x=value)
+	
+	def on_L3_right(self, value):
+		update_drive(x=value)
+	
+	def on_L3_x_at_rest(self, ):
+		update_drive(s=value)
+		
+	def on_L3_y_at_rest(self, ):
+		update_drive(y=value)
+	
+		
+controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
+controller.listen(timeout=60)
+print("Let's go!")
