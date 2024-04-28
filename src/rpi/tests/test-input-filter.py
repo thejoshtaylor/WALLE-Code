28f2726f286f3558d95ef4398d229cf3662ec803
Joshua Taylor <theofficialjoshuataylor@gmail.com>
2024-04-27 18:02:27 -0700

    Fine tuned sampling rate

--- src/rpi/tests/test-input-filter.py
+++ src/rpi/tests/test-input-filter.py
@@ -67,7 +67,7 @@ def threadPrint():
     while True:
         updatePIDs()
         printTable()
-        time.sleep(0.04)
+        time.sleep(0.03)
         retractCursor()
 
 class MyController(Controller):
