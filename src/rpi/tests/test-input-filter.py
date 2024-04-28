d0b68999a2a20a7173029c1d59506dd21b1bac3b
Joshua Taylor <theofficialjoshuataylor@gmail.com>
2024-04-27 17:42:06 -0700

    Updated input filtering test for cleaner output

--- src/rpi/tests/test-input-filter.py
+++ src/rpi/tests/test-input-filter.py
@@ -1,22 +1,68 @@
 from pyPS4Controller.controller import Controller
 from simple_pid import PID
 
+import time
+
+l3_vert_pre = 0
+l3_horz_pre = 0
+r3_vert_pre = 0
+r3_horz_pre = 0
+r2_pre = 0
+l2_pre = 0
+
+l3_vert = 0
+l3_horz = 0
+r3_vert = 0
+r3_horz = 0
+r2 = 0
+l2 = 0
+
+pid_objects = {
+    'l3_vert': PID(0.9, 0.001, 0.1),
+    'l3_horz': PID(0.9, 0.001, 0.1),
+
+    'r3_vert': PID(0.9, 0.001, 0.1),
+    'r3_horz': PID(0.9, 0.001, 0.1),
+
+    'r2': PID(0.9, 0.001, 0.1),
+    'l2': PID(0.9, 0.001, 0.1),
+}
+
+def printTable():
+    print('|', '-'*10, '|', '-'*10, '|', '-'*10, '|', '-'*10, '|', '-'*10, '|', '-'*10, '|', '-'*10, '|', sep='-')
+    print('|', ' '*10, '| {0:10} | {1:10} | {2:10} | {3:10} | {4:10} | {5:10} |'.format('L3_vert', 'L3_horz', 'R3_vert', 'R3_horz', 'R2', 'L2'))
+    print('|', '-'*10, '|', '-'*10, '|', '-'*10, '|', '-'*10, '|', '-'*10, '|', '-'*10, '|', '-'*10, '|', sep='-')
+    print('|', '   raw    ', '| {0:10.2f} | {1:10.2f} | {2:10.2f} | {3:10.2f} | {4:10.2f} | {5:10.2f} |'.format(l3_vert_pre, l3_horz_pre, r3_vert_pre, r3_horz_pre, r2_pre, l2_pre))
+    print('|', '   filt   ', '| {0:10.2f} | {1:10.2f} | {2:10.2f} | {3:10.2f} | {4:10.2f} | {5:10.2f} |'.format(l3_vert, l3_horz, r3_vert, r3_horz, r2, l2))
+    print('|', '-'*10, '|', '-'*10, '|', '-'*10, '|', '-'*10, '|', '-'*10, '|', '-'*10, '|', '-'*10, '|', sep='-')
+	
+def retractCursor():
+    for i in range(0, 5):
+        print("\033[A                             \033[A")
+
+def updatePIDs():
+    global l3_vert_pre, l3_horz_pre, r3_vert_pre, r3_horz_pre, r2_pre, l2_pre
+    global l3_vert, l3_horz, r3_vert, r3_horz, r2, l2
+
+    l3_vert = pid_objects['l3_vert'](l3_vert_pre)
+    l3_horz = pid_objects['l3_horz'](l3_horz_pre)
+    r3_vert = pid_objects['r3_vert'](r3_vert_pre)
+    r3_horz = pid_objects['r3_horz'](r3_horz_pre)
+    r2 = pid_objects['r2'](r2_pre)
+    l2 = pid_objects['l2'](l2_pre)
+	
+def threadPrint():
+    while True:
+        updatePIDs()
+        printTable()
+        time.sleep(0.1)
+        retractCursor()
+
 class MyController(Controller):
 	def __init__(self, **kwargs):
 		Controller.__init__(self, **kwargs)
 		self.last_drive_x = 0
 		self.last_drive_y = 0
-		
-		self.pid_objects = {
-			'l3_vert': PID(0.9, 0.001, 0.1),
-			'l3_horz': PID(0.9, 0.001, 0.1),
-
-			'r3_vert': PID(0.9, 0.001, 0.1),
-			'r3_horz': PID(0.9, 0.001, 0.1),
-
-			'r2': PID(0.9, 0.001, 0.1),
-			'l2': PID(0.9, 0.001, 0.1),
-		}
 
 	def input_filter(self, value, name):
 		if name not in self.pid_objects:
@@ -26,56 +72,79 @@ class MyController(Controller):
 
 	# Left stick (tank drive)
 	def on_L3_up(self, value):
-		print('L3_vert', value, self.input_filter(value, 'l3_vert'))
+		global l3_vert_pre
+		l3_vert_pre = value
 	
 	def on_L3_down(self, value):
-		print('L3_vert', value, self.input_filter(value, 'l3_vert'))
+		global l3_vert_pre
+		l3_vert_pre = value
 		
 	def on_L3_y_at_rest(self, ):
-		print('L3_vert', 0, self.input_filter(0, 'l3_vert'))
+		global l3_vert_pre
+		l3_vert_pre = 0
 
 	def on_L3_left(self, value):
-		print('L3_horz', value, self.input_filter(value, 'l3_horz'))
+		global l3_horz_pre
+		l3_horz_pre = value
 		
 	def on_L3_right(self, value):
-		print('L3_horz', value, self.input_filter(value, 'l3_horz'))
+		global l3_horz_pre
+		l3_horz_pre = value
 	
 	def on_L3_x_at_rest(self, ):
-		print('L3_horz', 0, self.input_filter(0, 'l3_horz'))
+		global l3_horz_pre
+		l3_horz_pre = 0
 
 	# Right stick (arm elevation)
 	def on_R3_up(self, value):
-		print('R3_vert', value, self.input_filter(value, 'r3_vert'))
+		global r3_vert_pre
+		r3_vert_pre = value
 	
 	def on_R3_down(self, value):
-		print('R3_vert', value, self.input_filter(value, 'r3_vert'))
+		global r3_vert_pre
+		r3_vert_pre = value
 		
 	def on_R3_y_at_rest(self, ):
-		print('R3_vert', 0, self.input_filter(0, 'r3_vert'))
+		global r3_vert_pre
+		r3_vert_pre = 0
 
 	def on_R3_left(self, value):
-		print('R3_horz', value, self.input_filter(value, 'r3_horz'))
+		global r3_horz_pre
+		r3_horz_pre = value
 		
 	def on_R3_right(self, value):
-		print('R3_horz', value, self.input_filter(value, 'r3_horz'))
+		global r3_horz_pre
+		r3_horz_pre = value
 	
 	def on_R3_x_at_rest(self, ):
-		print('R3_horz', 0, self.input_filter(0, 'r3_horz'))
+		global r3_horz_pre
+		r3_horz_pre = 0
 
 	# Right trigger (arm extension)
 	def on_R2_press(self, value):
-		print('R2', value, self.input_filter(value, 'r2'))
+		global r2_pre
+		r2_pre = value
 
 	def on_R2_release(self, ):
-		print('R2', 0, self.input_filter(0, 'r2'))
+		global r2_pre
+		r2_pre = 0
 
 	# Left stick (shredder control [TEMPORARY])
 	def on_L2_press(self, value):
-		print('L2', value, self.input_filter(value, 'l2'))
+		global l2_pre
+		l2_pre = value
 
 	def on_L2_release(self, ):
-		print('L2', 0, self.input_filter(0, 'l2'))
+		global l2_pre
+		l2_pre = 0
 		
+
+print()
+
+# Start the thread
+import threading
+thread = threading.Thread(target=threadPrint)
+thread.start()
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 print(controller.listen(timeout=60))
